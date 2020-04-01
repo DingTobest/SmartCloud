7b9c11d606c37ba8f5595005dd80fe7ea6bf82a1
DingTobest <ding798513@163.com>
2020-04-01 16:38:35 +0800

    通过中位数的方法重新计算指数估值数据

--- sctk/history_data_service.py
+++ sctk/history_data_service.py
@@ -21,8 +21,6 @@ def import_history_csv(table_name, csv_file_path):
 
     engine = create_engine('mysql+pymysql://root:root@127.0.0.1/sc_stock?charset=utf8')
 
-    # engine = create_engine('mysql+pymysql://root:root@140.143.154.20/sc_stock?charset=utf8')
-
     drop_table_sql = "DROP TABLE IF EXISTS `%table_name%`;"
     create_table_sql = '''CREATE TABLE `%table_name%` (
           `trade_date` date DEFAULT NULL,
@@ -46,21 +44,21 @@ def import_history_csv(table_name, csv_file_path):
     index_cp_df.to_sql(table_name, engine, if_exists='append', index=False)
 
 if __name__ == '__main__':
-    # import_history_csv('000016.XSHG', 'd:\\sz50.csv')
-    # import_history_csv('000905.XSHG', 'd:\\zz500.csv')
-    # import_history_csv('QZZS.SC', 'd:\\szzs.csv')
-    import_history_csv('399673.XSHE', 'D:\\historyanalysisdata\\chuangyeban50.399673.XSHE.csv')
-    import_history_csv('399006.XSHE', 'D:\\historyanalysisdata\\chuangyebanzhishu.399006.XSHE.csv')
-    import_history_csv('000300.XSHG', 'D:\\historyanalysisdata\\hushen300.000300.XSHG.csv')
-    import_history_csv('000992.XSHG', 'D:\\historyanalysisdata\\quanzhijinrong.000992.XSHG.csv')
-    import_history_csv('000993.XSHG', 'D:\\historyanalysisdata\\quanzhixinxi.000993.XSHG.csv')
-    import_history_csv('000991.XSHG', 'D:\\historyanalysisdata\\quanzhiyiyao.000991.XSHG.csv')
-    import_history_csv('000807.XSHG', 'D:\\historyanalysisdata\\shipinyinliao.000807.XSHG.csv')
-    import_history_csv('399610.XSHE', 'D:\\historyanalysisdata\\tmt50.399610.XSHE.csv')
-    import_history_csv('399550.XSHE', 'D:\\historyanalysisdata\\yangshi50.399550.XSHE.csv')
-    import_history_csv('000852.XSHG', 'D:\\historyanalysisdata\\zhongzheng1000.000852.XSHG.csv')
-    import_history_csv('000922.XSHG', 'D:\\historyanalysisdata\\zhongzhenghongli.000922.XSHG.csv')
-    import_history_csv('399967.XSHE', 'D:\\historyanalysisdata\\zhongzhengjungong.399967.XSHE.csv')
-    import_history_csv('399812.XSHE', 'D:\\historyanalysisdata\\zhongzhengyanglao.399812.XSHE.csv')
-    import_history_csv('399417.XSHE', 'D:\\historyanalysisdata\\国证新能源汽车指数.399417.XSHE.csv')
-    import_history_csv('000990.XSHG', 'D:\\historyanalysisdata\\全指消费.000990.XSHG.csv')
+    # import_history_csv('QZZS.SC', 'd:\\QZZS.SC.csv')
+    import_history_csv('000016.XSHG', 'd:\\newdata\\000016.XSHG.csv')
+    # import_history_csv('000905.XSHG', 'd:\\newdata\\000905.XSHG.csv')
+    # import_history_csv('399673.XSHE', 'D:\\newdata\\399673.XSHE.csv')
+    # import_history_csv('399006.XSHE', 'D:\\newdata\\399006.XSHE.csv')
+    # import_history_csv('000300.XSHG', 'D:\\newdata\\000300.XSHG.csv')
+    # import_history_csv('000992.XSHG', 'D:\\newdata\\000992.XSHG.csv')
+    # import_history_csv('000993.XSHG', 'D:\\newdata\\000993.XSHG.csv')
+    # import_history_csv('000991.XSHG', 'D:\\newdata\\000991.XSHG.csv')
+    # import_history_csv('000807.XSHG', 'D:\\newdata\\000807.XSHG.csv')
+    # import_history_csv('399610.XSHE', 'D:\\newdata\\399610.XSHE.csv')
+    # import_history_csv('399550.XSHE', 'D:\\newdata\\399550.XSHE.csv')
+    # import_history_csv('000852.XSHG', 'D:\\newdata\\000852.XSHG.csv')
+    # import_history_csv('000922.XSHG', 'D:\\newdata\\000922.XSHG.csv')
+    # import_history_csv('399967.XSHE', 'D:\\newdata\\399967.XSHE.csv')
+    # import_history_csv('399812.XSHE', 'D:\\newdata\\399812.XSHE.csv')
+    # import_history_csv('399417.XSHE', 'D:\\newdata\\399417.XSHE.csv')
+    # import_history_csv('000990.XSHG', 'D:\\newdata\\000990.XSHG.csv')
--- sctk/index_fundamental_analysis.py
+++ sctk/index_fundamental_analysis.py
@@ -5,9 +5,39 @@
 
 # 计算指数市盈率(PE)
 # df中包含所有股票的pe_ttm信息
+# 计算方法：过滤调亏损股票（市盈率小于0），对剩余股票市盈率取中位数
+# 通过中位数的形式，衡量市场主体估值水平
+def calc_index_pe_ttm(df):
+    df_pe = df[df['pe_ratio'] > 0]
+    index_pe = df_pe['pe_ratio'].median()
+    return index_pe
+
+# 计算指数市净率(PB)
+# df中包含所有股票的pb_ttm信息
+# 计算方法：过滤调亏损股票（市净率小于0），对剩余股票市净率取中位数
+# 通过中位数的形式，衡量市场主体估值水平
+def calc_index_pb_ttm(df):
+    df_pb = df[df['pb_ratio'] > 0]
+    index_pb = df_pb['pb_ratio'].median()
+    return index_pb
+
+# 计算指数市销率(PS)
+# df中包含所有股票的ps_ttm信息
+# 计算方法：过滤调亏损股票（市销率小于0），对剩余股票市销率取中位数
+# 通过中位数的形式，衡量市场主体估值水平
+def calc_index_ps_ttm(df):
+    df_ps = df[df['ps_ratio'] > 0]
+    index_ps = df_ps['ps_ratio'].median()
+    return index_ps
+
+# 暂时废弃以下计算方式
+# 以下计算方式会导致低市盈率股票的权重过大，计算出来的市盈率过小，不符合对指数市盈率的观察需求
+
+# 计算指数市盈率(PE)
+# df中包含所有股票的pe_ttm信息
 # 计算方法：对所有个股的pe_ttm数据取倒数，剔除掉小于0的数据，通过总和除以所有的股票数，再取倒数，形成指数pe_ttm
 # 通过取倒数的形式，大大减小了超高PE的股票对指数PE的影响
-def calc_index_pe_ttm(df):
+def calc_index_pe_ttm_old(df):
     df['r_pe_ttm'] = 1 / df['pe_ratio']
     df2 = df[df['r_pe_ttm'] > 0]
     r_index_pe = df2['r_pe_ttm'].sum() / len(df)
@@ -18,18 +48,18 @@ def calc_index_pe_ttm(df):
 # df中包含所有股票的pb_ttm信息
 # 计算方法：对所有个股的pb_ttm数据取倒数，剔除掉小于0的数据，通过总和除以所有的股票数，再取倒数，形成指数pb_ttm
 # 通过取倒数的形式，大大减小了超高PB的股票对指数PB的影响
-def calc_index_pb_ttm(df):
+def calc_index_pb_ttm_old(df):
     df['r_pb_ttm'] = 1 / df['pb_ratio']
     df2 = df[df['r_pb_ttm'] > 0]
     r_index_pb = df2['r_pb_ttm'].sum() / len(df)
     index_pb = 1 / r_index_pb
     return index_pb
 
-# 计算指数市净率(PB)
-# df中包含所有股票的pb_ttm信息
-# 计算方法：对所有个股的pb_ttm数据取倒数，剔除掉小于0的数据，通过总和除以所有的股票数，再取倒数，形成指数pb_ttm
-# 通过取倒数的形式，大大减小了超高PB的股票对指数PB的影响
-def calc_index_ps_ttm(df):
+# 计算指数市销率(PS)
+# df中包含所有股票的ps_ttm信息
+# 计算方法：对所有个股的ps_ttm数据取倒数，剔除掉小于0的数据，通过总和除以所有的股票数，再取倒数，形成指数ps_ttm
+# 通过取倒数的形式，大大减小了超高PS的股票对指数PB的影响
+def calc_index_ps_ttm_old(df):
     df['r_ps_ttm'] = 1 / df['ps_ratio']
     df2 = df[df['r_ps_ttm'] > 0]
     r_index_ps = df2['r_ps_ttm'].sum() / len(df)
